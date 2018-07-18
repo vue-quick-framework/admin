ea76d866d3f63d2dc1c5418a5e16b1e626a4c675
mars <578595193@qq.com>
2018-07-18 22:26:00 +0800

    add Login

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,12 +2,40 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAuth from '@websanova/vue-auth'
 
-let cusAxios = axios
+const cusAxios = axios.create({
+  baseURL: process.env.API_HOST,
+  timeout: 15000
+})
+// cusAxios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+// Add a request interceptor
+cusAxios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// Add a response interceptor
+cusAxios.interceptors.response.use(function (response) {
+  // Do something with response data
+  return response
+}, function (error) {
+  // Do something with response error
+  // alert(error);
+  return Promise.reject(error)
+})
+
 Vue.axios = cusAxios
 Vue.use(VueAuth, {
-  auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+  auth: require('@websanova/vue-auth/drivers/auth/devise.js'),
   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
-  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js')
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+  authType: 'devise',
+  loginData: { url: process.env.API_HOST + '/auth_admin/sign_in', method: 'POST', redirect: '/' },
+  logoutData: { url: process.env.API_HOST + '/auth_admin/sign_out', method: 'DELETE', redirect: '/login' },
+  refreshData: { url: process.env.API_HOST + '/auth_admin/validate_token', method: 'GET', enabled: true, interval: 30 },
+  fetchData: { url: process.env.API_HOST + '/auth_admin/validate_token', method: 'GET', enabled: true }
 })
 
 export default {
@@ -15,7 +43,7 @@ export default {
     Object.defineProperties(Vue.prototype, {
       axios: {
         get () {
-          return axios
+          return cusAxios
         }
       }
     })
